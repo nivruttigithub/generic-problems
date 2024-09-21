1fd7b3dac93c35c02d8390d7e7a1852c830264db
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-21 11:07:53 +0530

    Added : Given Max Number at 1st Position return the Same Number

--- src/test/java/com/bridgelabz/genericproblems/MaximumNumberTest.java
+++ src/test/java/com/bridgelabz/genericproblems/MaximumNumberTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static junit.framework.Assert.assertEquals;
 
 public class MaximumNumberTest {
+    //TC1.1 : Given Max Number at 1st Position return the Same Number
     @Test
     public void WhenGivenMaxAtFirstPosition_ShouldReturnMax() {
         Assertions.assertEquals(15, MaximumNumber.findMax(15, 5, 3),"Max should be 15");
