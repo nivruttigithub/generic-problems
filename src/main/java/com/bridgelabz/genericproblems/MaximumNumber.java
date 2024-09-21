b26606a16f9b758773ff12d1e987550eb6f635f5
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-21 17:57:38 +0530

    Refactor : Refactor all the 3 to One Generic Method and find the maximum

--- src/main/java/com/bridgelabz/genericproblems/MaximumNumber.java
+++ src/main/java/com/bridgelabz/genericproblems/MaximumNumber.java
@@ -2,48 +2,30 @@ package com.bridgelabz.genericproblems;
 
 public class MaximumNumber {
 
-    public static Float findMax(Float num1, Float num2, Float num3) {
-        Float max = num1;
+public static <T extends Comparable<T>> T findMax(T num1, T num2, T num3) {
+    T max = num1;
 
-        if (num2.compareTo(max) > 0) {
-            max = num2;
-        }
-        if (num3.compareTo(max) > 0) {
-            max = num3;
-        }
-
-        return max;
+    if (num2.compareTo(max) > 0) {
+        max = num2;
     }
-    public static String findMax(String str1, String str2, String str3) {
-        String max = str1;
-
-        if (str2.compareTo(max) > 0) {
-            max = str2;
-        }
-        if (str3.compareTo(max) > 0) {
-            max = str3;
-        }
-
-        return max;
+    if (num3.compareTo(max) > 0) {
+        max = num3;
     }
+
+    return max;
+}
     public static void main(String[] args) {
 
-        Float num1 = 10.5f, num2 = 5.2f, num3 = 3.1f;
-        System.out.println("Max of 10.5, 5.2, 3.1 is : " + findMax(num1, num2, num3));
+        System.out.println("Max of 10, 5, 3 is: " + findMax(10, 5, 3));
+        System.out.println("Max of (3, 12, 7) is: " + findMax(3, 12, 7));
+        System.out.println("Max of (2, 4, 10) is: " + findMax(2, 4, 10));
 
-        Float a2 = 3.3f, b2 = 12.8f, c2 = 7.4f;
-        System.out.println("Max of (3.3, 12.8, 7.4) is: " + findMax(a2, b2, c2));
+        System.out.println("Max of 10.5, 5.2, 3.1 is: " + findMax(10.5f, 5.2f, 3.1f));
+        System.out.println("Max of (3.3, 12.8, 7.4) is: " + findMax(3.3f, 12.8f, 7.4f));
+        System.out.println("Max of (2.2, 4.4, 10.1) is: " + findMax(2.2f, 4.4f, 10.1f));
 
-        Float a3 = 2.2f, b3 = 4.4f, c3 = 10.1f;
-        System.out.println("Max of (2.2, 4.4, 10.1) is: " + findMax(a3, b3, c3));
-
-        String s1 = "Zebra", s2 = "Elephant", s3 = "Lion";
-        System.out.println("Max of (Zebra, Elephant, Lion) is: " + findMax(s1, s2, s3));
-
-        String str1 = "Apple", str2 = "Peach", str3 = "Banana";
-        System.out.println("Max of (Apple, Peach, Banana) is : " + findMax(str1, str2, str3));
-
-        String st1 = "Cherry", st2 = "Apricot", st3 = "Mango";
-        System.out.println("Max of (Cherry, Apricot, Mango) is: " + findMax(st1, st2, st3));
-    }
+        System.out.println("Max of (Zebra, Elephant, Lion) is: " + findMax("Zebra", "Elephant", "Lion"));
+        System.out.println("Max of (Apple, Peach, Banana) is: " + findMax("Apple", "Peach", "Banana"));
+        System.out.println("Max of (Cherry, Apricot, Mango) is: " + findMax("Cherry", "Apricot", "Mango"));
+}
 }
--- src/test/java/com/bridgelabz/genericproblems/MaximumNumberTest.java
+++ src/test/java/com/bridgelabz/genericproblems/MaximumNumberTest.java
@@ -6,6 +6,21 @@ import org.junit.jupiter.api.Test;
 public class MaximumNumberTest {
     // TC1.1 : Given Max Number at 1st Position return the Same Number
     @Test
+    public void WhenGivenMaxAtFirstPosition_ShouldReturnMax_Integer() {
+        Assertions.assertEquals(15, MaximumNumber.findMax(15, 5, 3), "Max should be 15");
+    }
+   // TC1.2 : Given Max Number at 2nd Position return the Same Number
+    @Test
+    public void WhenGivenMaxAtSecondPosition_ShouldReturnMax_Integer() {
+        Assertions.assertEquals(20, MaximumNumber.findMax(5, 20, 15), "Max should be 20");
+    }
+    // TC1.3 : Given Max Number at 3rd Position return the Same Number
+    @Test
+    public void WhenGivenMaxAtThirdPosition_ShouldReturnMax_Integer() {
+        Assertions.assertEquals(30, MaximumNumber.findMax(5, 15, 30), "Max should be 30");
+    }
+    // TC1.1 : Given Max Number at 1st Position return the Same Number
+    @Test
     public void WhenGivenMaxAtFirstPosition_ShouldReturnMax() {
         Assertions.assertEquals(15.5f, MaximumNumber.findMax(15.5f, 5.0f, 3.0f), "Max should be 15.5");
     }
