390c1c3c46627c37c91454ec0b4b2058ee285f04
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-21 09:32:29 +0530

    Added : Given an array of Integer Double and Character write a program to print the Same

--- src/main/java/com/bridgelabz/genericproblems/PrintArray.java
+++ src/main/java/com/bridgelabz/genericproblems/PrintArray.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.genericproblems;
 
 public class PrintArray {
-    // Method to print Integer array
+
     public static void toPrint(Integer[] intArray) {
         for (Integer element : intArray) {
             System.out.print(element + " ");
@@ -9,7 +9,6 @@ public class PrintArray {
         System.out.println();
     }
 
-    // Method to print Double array
     public static void toPrint(Double[] doubleArray) {
         for (Double element : doubleArray) {
             System.out.print(element + " ");
@@ -17,7 +16,6 @@ public class PrintArray {
         System.out.println();
     }
 
-    // Method to print Character array
     public static void toPrint(Character[] charArray) {
         for (Character element : charArray) {
             System.out.print(element + " ");
@@ -26,12 +24,11 @@ public class PrintArray {
     }
 
     public static void main(String[] args) {
-        // Create arrays of Integer, Double, and Character
+
         Integer[] intArray = {1, 2, 3, 4, 5};
         Double[] doubleArray = {1.1, 2.2, 3.3, 4.4};
         Character[] charArray = {'H', 'E', 'L', 'L', 'O'};
-
-        // Call toPrint method to print each array
+        
         System.out.print("Integer Array: ");
         toPrint(intArray);
 
