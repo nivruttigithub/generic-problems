83e96e45a7ae3b6c782f60991a4bcf10c7ac1272
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-21 10:32:30 +0530

    Added : Given 3 Integers find the maximum

---
+++ src/main/java/com/bridgelabz/genericproblems/MaximumNumber.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.genericproblems;
+
+public class MaximumNumber {
+    public static Integer findMax(Integer num1, Integer num2, Integer num3) {
+        Integer max = num1; 
+
+        if (num2.compareTo(max) > 0) {
+            max = num2;
+        }
+        if (num3.compareTo(max) > 0) {
+            max = num3;
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Max of 10, 5, 3 is : " + findMax(10, 5, 3));
+
+    }
+}
