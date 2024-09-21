f64e5b8de49191acdd58bfcb6d70ce2717e286e1
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-21 11:15:44 +0530

    Added : Given Max Number at 2nd Position return the Same Number

--- src/main/java/com/bridgelabz/genericproblems/MaximumNumber.java
+++ src/main/java/com/bridgelabz/genericproblems/MaximumNumber.java
@@ -15,8 +15,12 @@ public class MaximumNumber {
     }
 
     public static void main(String[] args) {
-
+        //1st Position
+        Integer num1 = 10, num2 = 5, num3 = 3;
         System.out.println("Max of 10, 5, 3 is : " + findMax(10, 5, 3));
+        //2nd Position
+        Integer a2 = 3, b2 = 12, c2 = 7;
+        System.out.println("Max of (3, 12, 7) is: " + findMax(a2, b2, c2));
 
     }
 }
--- src/test/java/com/bridgelabz/genericproblems/MaximumNumberTest.java
+++ src/test/java/com/bridgelabz/genericproblems/MaximumNumberTest.java
@@ -6,8 +6,15 @@ import org.junit.jupiter.api.Test;
 import static junit.framework.Assert.assertEquals;
 
 public class MaximumNumberTest {
+    //TC1.1 : Given Max Number at 1st Position return the Same Number
     @Test
     public void WhenGivenMaxAtFirstPosition_ShouldReturnMax() {
         Assertions.assertEquals(15, MaximumNumber.findMax(15, 5, 3),"Max should be 15");
     }
+
+    //TC1.2 : Given Max Number at 2nd Position return the Same Number
+    @Test
+    public void WhenGivenMaxAtSecondPosition_ShouldReturnMax() {
+        Assertions.assertEquals(20, MaximumNumber.findMax(5, 20, 15), "Max should be 20");
+         }
 }
