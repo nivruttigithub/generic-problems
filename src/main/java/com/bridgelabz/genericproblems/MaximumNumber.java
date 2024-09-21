ef3dc51436bfd6d301fd7d38d560cea9ca298b6c
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-21 20:39:39 +0530

    Refactor : to create Generic Class to take in 3 variables of Generic Type

--- src/main/java/com/bridgelabz/genericproblems/MaximumNumber.java
+++ src/main/java/com/bridgelabz/genericproblems/MaximumNumber.java
@@ -1,31 +1,40 @@
 package com.bridgelabz.genericproblems;
 
-public class MaximumNumber {
+public class MaximumNumber <T extends Comparable<T>> {
+    private T num1, num2, num3;
 
-public static <T extends Comparable<T>> T findMax(T num1, T num2, T num3) {
-    T max = num1;
-
-    if (num2.compareTo(max) > 0) {
-        max = num2;
-    }
-    if (num3.compareTo(max) > 0) {
-        max = num3;
+    public MaximumNumber(T num1, T num2, T num3) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
     }
 
-    return max;
-}
+    public T findMax() {
+        return MaximumNumber.findMax(num1, num2, num3);
+    }
+
+    public static <T extends Comparable<T>> T findMax(T num1, T num2, T num3) {
+        T max = num1;
+
+        if (num2.compareTo(max) > 0) {
+            max = num2;
+        }
+        if (num3.compareTo(max) > 0) {
+            max = num3;
+        }
+
+        return max;
+    }
+
     public static void main(String[] args) {
 
-        System.out.println("Max of 10, 5, 3 is: " + findMax(10, 5, 3));
-        System.out.println("Max of (3, 12, 7) is: " + findMax(3, 12, 7));
-        System.out.println("Max of (2, 4, 10) is: " + findMax(2, 4, 10));
+        MaximumNumber<Integer> integerMax = new MaximumNumber<>(10, 5, 3);
+        System.out.println("Max of 10, 5, 3 is: " + integerMax.findMax());  // Output: 10
 
-        System.out.println("Max of 10.5, 5.2, 3.1 is: " + findMax(10.5f, 5.2f, 3.1f));
-        System.out.println("Max of (3.3, 12.8, 7.4) is: " + findMax(3.3f, 12.8f, 7.4f));
-        System.out.println("Max of (2.2, 4.4, 10.1) is: " + findMax(2.2f, 4.4f, 10.1f));
+        MaximumNumber<Float> floatMax = new MaximumNumber<>(10.5f, 5.2f, 3.1f);
+        System.out.println("Max of 10.5, 5.2, 3.1 is: " + floatMax.findMax());  // Output: 10.5
 
-        System.out.println("Max of (Zebra, Elephant, Lion) is: " + findMax("Zebra", "Elephant", "Lion"));
-        System.out.println("Max of (Apple, Peach, Banana) is: " + findMax("Apple", "Peach", "Banana"));
-        System.out.println("Max of (Cherry, Apricot, Mango) is: " + findMax("Cherry", "Apricot", "Mango"));
-}
+        MaximumNumber<String> stringMax = new MaximumNumber<>("Zebra", "Elephant", "Lion");
+        System.out.println("Max of (Zebra, Elephant, Lion) is: " + stringMax.findMax());
+    }
 }
--- src/test/java/com/bridgelabz/genericproblems/MaximumNumberTest.java
+++ src/test/java/com/bridgelabz/genericproblems/MaximumNumberTest.java
@@ -4,53 +4,24 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MaximumNumberTest {
-    // TC1.1 : Given Max Number at 1st Position return the Same Number
+    //Integer
     @Test
-    public void WhenGivenMaxAtFirstPosition_ShouldReturnMax_Integer() {
-        Assertions.assertEquals(15, MaximumNumber.findMax(15, 5, 3), "Max should be 15");
-    }
-   // TC1.2 : Given Max Number at 2nd Position return the Same Number
-    @Test
-    public void WhenGivenMaxAtSecondPosition_ShouldReturnMax_Integer() {
-        Assertions.assertEquals(20, MaximumNumber.findMax(5, 20, 15), "Max should be 20");
-    }
-    // TC1.3 : Given Max Number at 3rd Position return the Same Number
-    @Test
-    public void WhenGivenMaxAtThirdPosition_ShouldReturnMax_Integer() {
-        Assertions.assertEquals(30, MaximumNumber.findMax(5, 15, 30), "Max should be 30");
-    }
-    // TC1.1 : Given Max Number at 1st Position return the Same Number
-    @Test
-    public void WhenGivenMaxAtFirstPosition_ShouldReturnMax() {
-        Assertions.assertEquals(15.5f, MaximumNumber.findMax(15.5f, 5.0f, 3.0f), "Max should be 15.5");
-    }
-    // TC1.2 : Given Max Number at 2nd Position return the Same Number
-    @Test
-    public void WhenGivenMaxAtSecondPosition_ShouldReturnMax() {
-        Assertions.assertEquals(20.2f, MaximumNumber.findMax(5.0f, 20.2f, 15.0f), "Max should be 20.2");
+    public void WhenGivenThreeIntegers_ShouldReturnMax_Integer() {
+        MaximumNumber<Integer> maxNum = new MaximumNumber<>(10, 20, 15);
+        Assertions.assertEquals(20, maxNum.findMax(), "Max should be 20");
     }
 
-    // TC1.3 : Given Max Number at 3rd Position return the Same Number
+    //Float
     @Test
-    public void WhenGivenMaxAtThirdPosition_ShouldReturnMax() {
-        Assertions.assertEquals(30.3f, MaximumNumber.findMax(5.0f, 15.0f, 30.3f), "Max should be 30.3");
+    public void WhenGivenThreeFloats_ShouldReturnMax_Float() {
+        MaximumNumber<Float> maxNum = new MaximumNumber<>(5.5f, 10.5f, 9.9f);
+        Assertions.assertEquals(10.5f, maxNum.findMax(), "Max should be 10.5");
     }
 
-    // TC1.1 : Given Max String at 1st Position return the Same String
+    //String
     @Test
-    public void WhenGivenMaxStrAtFirstPosition_ShouldReturnMax() {
-        Assertions.assertEquals("Peach", MaximumNumber.findMax("Peach", "Apple", "Banana"), "Max should be Peach");
-    }
-
-    // TC1.2 : Given Max String at 2nd Position return the Same String
-    @Test
-    public void WhenGivenMaxStrAtSecondPosition_ShouldReturnMax() {
-        Assertions.assertEquals("Zebra", MaximumNumber.findMax("Lion", "Zebra", "Elephant"), "Max should be Zebra");
-    }
-
-    // TC1.3 : Given Max String at 3rd Position return the Same String
-    @Test
-    public void WhenGivenMaxStrAtThirdPosition_ShouldReturnMax() {
-        Assertions.assertEquals("Mango", MaximumNumber.findMax("Cherry", "Apricot", "Mango"), "Max should be Mango");
+    public void WhenGivenThreeStrings_ShouldReturnMax_String() {
+        MaximumNumber<String> maxNum = new MaximumNumber<>("Apple", "Peach", "Banana");
+        Assertions.assertEquals("Peach", maxNum.findMax(), "Max should be Peach");
     }
 }
