42f53ad131db7d22c940d2a57c88c2c3237a468d
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-21 11:53:24 +0530

    Added : Given Three Floats find the maximum

--- src/main/java/com/bridgelabz/genericproblems/MaximumNumber.java
+++ src/main/java/com/bridgelabz/genericproblems/MaximumNumber.java
@@ -1,8 +1,9 @@
 package com.bridgelabz.genericproblems;
 
 public class MaximumNumber {
-    public static Integer findMax(Integer num1, Integer num2, Integer num3) {
-        Integer max = num1; 
+
+    public static Float findMax(Float num1, Float num2, Float num3) {
+        Float max = num1;
 
         if (num2.compareTo(max) > 0) {
             max = num2;
@@ -13,18 +14,10 @@ public class MaximumNumber {
 
         return max;
     }
-
     public static void main(String[] args) {
 
-        //1st Position
-        Integer num1 = 10, num2 = 5, num3 = 3;
-        System.out.println("Max of 10, 5, 3 is : " + findMax(num1, num2, num3));
-        //2nd Position
-        Integer a2 = 3, b2 = 12, c2 = 7;
-        System.out.println("Max of (3, 12, 7) is: " + findMax(a2, b2, c2));
-        //3rd Position
-        Integer a3 = 2, b3 = 4, c3 = 10;
-        System.out.println("Max of (2, 4, 10) is: " + findMax(a3, b3,c3));
+        Float num1 = 10.5f, num2 = 5.2f, num3 = 3.1f;
+        System.out.println("Max of 10.5, 5.2, 3.1 is : " + findMax(num1, num2, num3));
 
     }
 }
