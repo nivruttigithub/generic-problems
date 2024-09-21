1e7afd1e5f2b0fb1226c26c3f71ba72ec5471c8f
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-09-21 20:56:07 +0530

    Added : Extend the max
    method to take
    
    more then three
    
    parameters

--- src/main/java/com/bridgelabz/genericproblems/MaximumNumber.java
+++ src/main/java/com/bridgelabz/genericproblems/MaximumNumber.java
@@ -1,40 +1,41 @@
 package com.bridgelabz.genericproblems;
 
+import java.util.Arrays;
+
 public class MaximumNumber <T extends Comparable<T>> {
-    private T num1, num2, num3;
+    private T[] elements;
 
-    public MaximumNumber(T num1, T num2, T num3) {
-        this.num1 = num1;
-        this.num2 = num2;
-        this.num3 = num3;
+    @SafeVarargs
+    public MaximumNumber(T... elements) {
+        this.elements = elements;
     }
 
-    public T findMax() {
-        return MaximumNumber.findMax(num1, num2, num3);
+    // Instance method to find the maximum of the instance variables
+    public T testMaximum() {
+        return MaximumNumber.testMaximum(elements);
     }
 
-    public static <T extends Comparable<T>> T findMax(T num1, T num2, T num3) {
-        T max = num1;
-
-        if (num2.compareTo(max) > 0) {
-            max = num2;
-        }
-        if (num3.compareTo(max) > 0) {
-            max = num3;
+    // Static method to find the maximum of multiple elements using varargs
+    @SafeVarargs
+    public static <T extends Comparable<T>> T testMaximum(T... elements) {
+        if (elements == null || elements.length == 0) {
+            throw new IllegalArgumentException("No elements provided");
         }
 
-        return max;
+        // Sorting the array and returning the maximum (last element)
+        Arrays.sort(elements);
+        return elements[elements.length - 1];  // Max is the last element after sorting
     }
 
     public static void main(String[] args) {
 
         MaximumNumber<Integer> integerMax = new MaximumNumber<>(10, 5, 3);
-        System.out.println("Max of 10, 5, 3 is: " + integerMax.findMax());  // Output: 10
+        System.out.println("Max of 10, 5, 3 is: " + integerMax.testMaximum());  // Output: 10
 
         MaximumNumber<Float> floatMax = new MaximumNumber<>(10.5f, 5.2f, 3.1f);
-        System.out.println("Max of 10.5, 5.2, 3.1 is: " + floatMax.findMax());  // Output: 10.5
+        System.out.println("Max of 10.5, 5.2, 3.1 is: " + floatMax.testMaximum());  // Output: 10.5
 
         MaximumNumber<String> stringMax = new MaximumNumber<>("Zebra", "Elephant", "Lion");
-        System.out.println("Max of (Zebra, Elephant, Lion) is: " + stringMax.findMax());
+        System.out.println("Max of (Zebra, Elephant, Lion) is: " + stringMax.testMaximum());
     }
 }
--- src/test/java/com/bridgelabz/genericproblems/MaximumNumberTest.java
+++ src/test/java/com/bridgelabz/genericproblems/MaximumNumberTest.java
@@ -8,20 +8,20 @@ public class MaximumNumberTest {
     @Test
     public void WhenGivenThreeIntegers_ShouldReturnMax_Integer() {
         MaximumNumber<Integer> maxNum = new MaximumNumber<>(10, 20, 15);
-        Assertions.assertEquals(20, maxNum.findMax(), "Max should be 20");
+        Assertions.assertEquals(20, maxNum.testMaximum(), "Max should be 20");
     }
 
     //Float
     @Test
     public void WhenGivenThreeFloats_ShouldReturnMax_Float() {
         MaximumNumber<Float> maxNum = new MaximumNumber<>(5.5f, 10.5f, 9.9f);
-        Assertions.assertEquals(10.5f, maxNum.findMax(), "Max should be 10.5");
+        Assertions.assertEquals(10.5f, maxNum.testMaximum(), "Max should be 10.5");
     }
 
     //String
     @Test
     public void WhenGivenThreeStrings_ShouldReturnMax_String() {
         MaximumNumber<String> maxNum = new MaximumNumber<>("Apple", "Peach", "Banana");
-        Assertions.assertEquals("Peach", maxNum.findMax(), "Max should be Peach");
+        Assertions.assertEquals("Peach", maxNum.testMaximum(), "Max should be Peach");
     }
 }
